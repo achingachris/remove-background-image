0b57e41a4c9b678e5a7304b441ffb08ea9b27190
chrisachinga <achinga.chris@gmail.com>
2022-08-01 18:21:35 +0300

    updated

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+module.exports = {
+  siteMetadata: {
+    title: `Cloudinary Images`,
+    description: `Get and Upload the images from Cloudinary`,
+    author: `Images`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/upload`,
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-cloudinary',
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        uploadFolder: 'Gatsby-images',
+        background_removal: "cloudinary_ai",
+      }
+    },
+    {
+      resolve: `gatsby-source-cloudinary`,
+      options: {
+        cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+        apiKey: process.env.CLOUDINARY_API_KEY,
+        apiSecret: process.env.CLOUDINARY_API_SECRET,
+        resourceType: `image`,
+      }
+    }
+  ],
+}
